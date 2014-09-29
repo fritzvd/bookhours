f316373f6f6b2771553638fb2dc885e047b551f4
fritzvd <f.vandeventer@gmail.com>
2014-09-29 14:28:12 +0200

    sso.conf now contains hours

--- index.js
+++ index.js
@@ -3,15 +3,17 @@ var HOURS = 32;
 
 
 var page = require('webpage').create();
-var myUrl = 'https://trs.lizard.net#' + HOURS;
 
-var user, pass;
+
+var user, pass, myUrl;
 
 var getCredentials = function () {
   data = fs.read('sso.conf')
 
   user = data.split('\n')[0].split('=')[1];
   pass = data.split('\n')[1].split('=')[1];
+  HOURS = data.split('\n')[2].split('=')[1];
+  myUrl = 'https://trs.lizard.net#' + HOURS;
 };
 
 var fs = require('fs');
@@ -35,10 +37,9 @@ var crawler = function (cred) {
   } else if (location.host === 'trs.lizard.net' ||
              location.host === 'trs.nelen-schuurmans.nl') {
     var nxt = document.getElementById('id_intern.1414');
-    var hash = location.hash.split('#')[1];
-    nxt.value = (hash) ? hash : 32;
+    nxt.value = cred.hours;
     $('input.btn').click();
-    return 'hours';    
+    return 'hours';
   } else {
     return 'location: ' + location.host
   }
@@ -48,13 +49,16 @@ var results = function (result) {
   console.log(result);
   if (result === 'logging in') {
     setTimeout(function () {    
-      var result = page.evaluate(crawler);
+      var result = page.evaluate(crawler, {hours: HOURS});
       results(result);
     }, 4000);
-  } else if (result === 'hours') {
+  } else if ('hours'){
     setTimeout(function () {
       phantom.exit();
     }, 4000);
+  } else {
+    console.log('something went wrong');
+    phantom.exit();
   }
 };
 
